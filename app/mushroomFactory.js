42bb7c7bd9bc269d0861477719a7b5034aae8495
Casey Dailey <casey@feasttogether.org>
2017-09-02 13:36:19 -0500

    processed data rendered to view in mushroom-list

--- app/mushroomFactory.js
+++ app/mushroomFactory.js
@@ -4,14 +4,24 @@ app.factory("mushroomFactory", function($q, $http, fbCreds){
 
     const url = fbCreds.databaseURL;
 
-    const arrayFrom = function(obj){
-        return Object.keys(obj).map((key)=> obj[key]);
-    }
+    // a utility to pull the values for each shroom from the array of objects
+    // whose one property is that mushroom's name and whose remaining values are yet another layer deep in their own object
+    // to do this, it maps each mushroom's values to a new array, assigning it a name property
+    // corresponding to it's original key
+    const getShrooms = data => {
+        return data.map((mushroom)=> {
+            mushroom[Object.keys(mushroom)[0]].name = Object.keys(mushroom)[0];
+            return mushroom[Object.keys(mushroom)[0]];
+        });
+    };
 
     const loadShrooms = function(){
         return $q((resolve, reject)=>{
             $http.get(`${url}/mushrooms.json`)
-                .then((data)=> resolve(arrayFrom(data.data)));
+                .then((data)=> {
+                    console.log("data.data", data.data);
+                    resolve(getShrooms(data.data));
+                });
         });
     };
 
