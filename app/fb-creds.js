0e89c8e4db7d4598e8e2b10a872464a9dc5a2077
Casey Dailey <casey@feasttogether.org>
2017-09-02 13:54:21 -0500

    oops

--- app/fb-creds.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-app.constant("fbCreds", {
-    apiKey: "AIzaSyB-AmytVW8xALYUw78E_jOKKG72xWExIXk",
-    authDomain: "general-purpose-64025.firebaseapp.com",
-    databaseURL: "https://general-purpose-64025.firebaseio.com",
-});
